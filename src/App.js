5d02360314f445982f2ff3513ddc0454b06ff163
Kelli Watkins <watkinskellil@gmail.com>
2024-01-09 14:25:58 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Navbar from './Navbar';
+import Home from './Home';
+
+function App() {
+  const title = 'Welcome to the New Blog'
+  const likes = 50
+  const link = 'http://google.com'
+
+  return (
+    <div className="App">
+      <Navbar />
+      <div className='content'>
+        <Home />
+      </div>
+    </div>
+  );
+}
+
+export default App;
